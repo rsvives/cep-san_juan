50920be047bebdcc5fbfa2590f9d509d6615c6e7
luis <l.edu31espinosa@gmail.com>
2025-05-14 15:10:02 +0200

    agencia/empresas/desempleados added

---
+++ agencia.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <?php include("menu.html"); ?>
+    </header>
+<h2>AGENCIA DE COLOCACIÓN</h2>
+<section class="container py-5">
+
+  <div class="text-center mb-4">
+    <img src="ruta-a-tu-imagen.jpg" alt="Imagen de la agencia" class="img-fluid">
+  </div>
+
+
+  <div class="p-4">
+    <p>
+      Bienvenidos a nuestra agencia de colocación. Nuestra finalidad consiste en proporcionar a los demandantes de empleo un puesto de trabajo adecuado a sus características profesionales y facilitar a las empresas los candidatos con el perfil más apropiado a sus requerimientos y necesidades.
+    </p>
+
+    <p>
+      La Agencia de Colocación es un instrumento gratuito que valora los perfiles, aptitudes, conocimientos y cualificaciones profesionales de los demandantes de empleo para proporcionarles una mejor empleabilidad y acceso al mercado laboral.
+    </p>
+
+    <p>
+      Asimismo piensa en la mejora de las empresas y su productividad facilitándoles las personas más adecuadas a sus requerimientos y necesidades.
+    </p>
+
+    <p>Para ello proporcionamos:</p>
+    <ul>
+      <li>Orientación</li>
+      <li>Formación para el empleo</li>
+      <li>Acompañamiento en la búsqueda de empleo</li>
+      <li>Intermediación laboral</li>
+    </ul>
+  </div>
+
+
+  <div class="text-center mt-4">
+    <a href="desempleados.php" class="btn btn-primary mx-2">Desempleados</a>
+    <a href="empresas.php" class="btn btn-secondary mx-2">Empresas</a>
+  </div>
+</section>
+
+    <footer class="bg-dark text-white py-5">
+        <?php
+        include("footer.php");
+        ?>
+    </footer>
+
+</body>
+
+</html>
---
+++ footer.php
@@ -0,0 +1,23 @@
+    <?php
+    $telefono1 = "985 216 462";
+    $telefono2 = "+34608741441"; ?>
+
+
+    <div class="container">
+        <div class="row justify-content-center text-center">
+            <div class="col-md-4 mb-3">
+                <a href="https://www.google.com/maps?..." class="text-white text-decoration-none d-block">
+                    C/ San Pedro Mestallón, 11 - Bajo
+                </a>
+            </div>
+            <div class="col-md-4 mb-3">
+                <a href="tel:<?php echo $telefono1 ?>" class="text-white text-decoration-none d-block">985 216 462</a>
+                <a href="tel:<?php echo $telefono2 ?>" class="text-white text-decoration-none d-block">+34 608 741 441</a>
+            </div>
+            <div class="d-flex justify-content-center gap-3">
+                <a href="http://www.facebook.com/pages/Centro-de-Estudios-Profesionales-San-Juan/319639151451384" class="text-white"><i class="fab fa-facebook fa-lg"></i></a>
+                <a href="https://twitter.com/CEPSANJUAN" class="text-white"><i class="fab fa-x-twitter fa-lg"></i></a>
+            </div>
+
+        </div>
+    </div>
